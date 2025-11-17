67f95d011401ad97e4fbedcdd56157292622183d
Laurent Guerard <laurent.guerard@unibas.ch>
2025-11-17 16:46:31 +0100

    Remove unneeded command from call

--- manage/assemble-fresh-fiji-installation.sh
+++ manage/assemble-fresh-fiji-installation.sh
@@ -90,7 +90,7 @@ fi
 echo ">>> adding required update sites..."
 # SCRIPT_DIR="$(dirname "$0")"
 set -x
-$FIJI_CMD --ij2 --headless --console \
+$FIJI_CMD \
     --run "add-update-sites.py" \
     sites_collection=\'"$UPD_SITES"\'
 set +x
